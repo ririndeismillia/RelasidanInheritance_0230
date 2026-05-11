821e36719c858650f420b49ef47ed10cbeb76560
ririndeismillia <ririndeismillia@gmail.com>
2026-05-11 21:06:49 +0700

    pembuatan constructor dan destructor dari class pasien

--- Asosiasi.cpp
+++ Asosiasi.cpp
@@ -8,4 +8,17 @@ class dokter;
 //deklarasi dan pembuatan class pasien
 class pasien {
 public:
+    //deklarasi dan pembuatan class pasien
+    string nama;
+    vector<dokter*> daftar_dokter;
+
+    //pembuatan constructor dan destructor dari class pasien
+    pasien(string pNama) :nama(pNama) {
+        cout << "Pasien \"" << nama << "\" ada\n";
+    };
+
+    ~pasien() {
+        cout << "Pasien \"" << nama << "\" tidak ada\n";
+    };
+
 }
