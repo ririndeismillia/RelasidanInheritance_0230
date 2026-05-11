94ab79180466e28e42ff77197be4f1665c1f2d09
ririndeismillia <ririndeismillia@gmail.com>
2026-05-11 21:20:42 +0700

    jantung.h

---
+++ jantung.h.cpp
@@ -0,0 +1,15 @@
+#ifndef JANTUNG_H
+#define JANTUNG_H
+//membuat class jantung di file header jantung.h
+class Jantung {
+public:
+   //membuat constructor dan destructor dari class jantung
+   Jantung() {
+       cout << "Jantung Ditambah\n";
+   }
+   ~jantung() {
+        cout << "Jantung Dimatikan\n";
+       }
+};
+
+#endif //!JANTUNG_H
