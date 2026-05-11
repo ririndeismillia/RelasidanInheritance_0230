8b6f675687a95d84da7e4175d7c5e3992bc48a7f
ririndeismillia <ririndeismillia@gmail.com>
2026-05-11 21:52:03 +0700

    pembuatan classs pelajar sebagai pewaris dari class orang

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -23,3 +23,33 @@ public:
     }
 };
 
+//pembuatan classs pelajar sebagai pewaris dari class orang
+class pelajar : public orang {
+public:
+     //deklarasi member variabel dari class pelajar
+     string sekolah;
+     
+     //pembuatan constructor dan desctructor dari class pelajar
+     pelajar(string pNama, string pSekolah) :orang(pNama), sekolah(pSekolah) {
+        cout << "Pelajar dibuat\n" << endl;
+     }
+
+     //pembuatan function perkenalan()
+     string perkenalan() {
+        return "Hallo, nama saya " + nama + "dari sekolah " + sekolah + "\n\n";
+
+     }
+
+    };
+
+    int main()
+    {
+        //deklarasi dan pemberian nilai untuk variabel object siswa1
+        pelajar siswa1("andi laksono","SMA 1 Bantul");
+        //panggilan function perkanalan dari class pelajar
+        cout << siswa1.perkenalan();
+        //pemanggil function warisan dari class orang kepada class object
+        cout << "Hasil = " << siswa1.jumlah(10,90) << endl;
+
+        return 0;
+    }
