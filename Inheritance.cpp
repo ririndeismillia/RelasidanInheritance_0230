849c8ed96026f720a5052a7dc2f65fd5fcf5f3f5
ririndeismillia <ririndeismillia@gmail.com>
2026-05-11 21:51:30 +0700

    pembuatan class orang

---
+++ Inheritance.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+//pembuatan class orang
+class orang {
+public:
+   //deklarasi member variabel dari class orang
+   string nama;
+   
+   //pembuatan constructor dan desctructor dari class orang 
+   orang(string pNama) :nama(pNama) {
+      cout << "Orang dibuat\n" << endl;
+   }
+
+   ~orang() {
+       cout << "Orang dihapus\n" << endl;
+   }
+
+   //pembuatan function jumlah
+   int jumlah(int a,int b) {
+      return a + b;
+    }
+};
+
