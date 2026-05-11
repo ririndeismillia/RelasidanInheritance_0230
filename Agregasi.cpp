58f62401295f93a77ddb32e65760523f94869537
ririndeismillia <ririndeismillia@gmail.com>
2026-05-11 18:40:28 +0700

    deklarasi dan pemberian nilai variabel pointer un tuk objrk dari class ibu dan anak

--- Agregasi.cpp
+++ Agregasi.cpp
@@ -7,3 +7,14 @@ using namespace std;
 #include "anak.h"
 #include "ibu.h"
 
+intmain()
+{
+    //deklarasi dan pemberian nilai variabel pointer un tuk objrk dari class ibu dan anak
+    Ibu* varIbu = new Ibu("rani");
+    Ibu* varIbu = new ibu("sari");
+    Ibu* varIbu = new Ibu("tono");
+    Ibu* varIbu = new Ibu("tiara");
+    Ibu* varIbu = new Ibu("dini");
+
+    
+}
