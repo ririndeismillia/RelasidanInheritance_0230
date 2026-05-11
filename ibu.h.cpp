0e62c7d0ac91e68fbabfcdb586a481b1ab4321c3
ririndeismillia <ririndeismillia@gmail.com>
2026-05-11 18:30:22 +0700

    deklarasi member variable pada class ibu

--- ibu.h.cpp
+++ ibu.h.cpp
@@ -8,4 +8,10 @@ class anak;
 //membuat class ibu pada file header ibu.h
 class ibu {
 public: 
+    //deklarasi member variable pada class ibu
+    string nama;
+    vector<anak*> daftar_anak;
+
+    
+      
 }
